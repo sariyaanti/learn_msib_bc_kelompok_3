625381eb0215381f98da22233ee0dea9bbddf90d
Sari Yanti Pandiangan <144535036+sariyaanti@users.noreply.github.com>
2023-09-13 07:52:58 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+let lastName = "Pandiangan";
+
+
+console.log(fisrt Name + " " + Middle Name + " " + last Name);
+
++
++ //variable, konstanta, konditional if else and switch case
++
++ let  kelas = A;
++
++ if (kelas >= 30) {
++   console.log (Jumlah Siswa : 30);
++ } else if (kelas A siswa < 30) {
++   console.log("Kelas : B");
++ } else if (kelas B siswa < 30) {
+    console.log (Kelas : C");
++ } else {
++    console.log (Kelas : D") ;
++ }
+
++ switch (kelas) {
++   case A:
++       console.log("Kelas : A");
++       break;
++   case B:
++       console.LOG(Kelas : B);
++       break
++   case:
++       console.LOG ("Kelas : C");
++       break;
++   case D:
++       console.log("Kelas : D");
++       break
++
++   default
++       break
++}
